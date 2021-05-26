2cb583b545d0d28928c11cc9bde3f8cf0befc4ee
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2021-05-26 11:47:27 -0600

    Added database schema, will start adding inserts.

---
+++ schema.sql
@@ -0,0 +1,31 @@
+-- Setting up database session. --
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
+
+-- Creating database tables. --
+CREATE TABLE department(
+ id INTEGER AUTO_INCREMENT,
+ name VARCHAR(30),
+ PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INTEGER AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(20, 2),
+    department_id INTEGER,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY(id)
+);
+
+-- Inserting values into database. --
+Insert Into department (name)
